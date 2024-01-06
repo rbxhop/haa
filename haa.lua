9098607585c71c6c84db7f4f9f53e61849d51815
rbxhop <136201096+rbxhop@users.noreply.github.com>
2024-01-06 10:30:54 +0100

    Update haa.lua

--- haa.lua
+++ haa.lua
@@ -221,7 +221,7 @@ local GetSnipes = function(Update)
 
 -- Rounding up to the nearest integer
 local roundedUpGetRap = math.ceil(GetRap)
-				print(v.Item)
+				
             print("Pet: " .. PetName .. " - Rarity: " .. Rarity .. " , - Rap: " .. roundedUpGetRap .. " , Price: " .. v.DiamondCost)
             print(v.Item["_data"].id, math.round(v.DiamondCost / (v.Item["_data"]["_am"] or 1)))
             if math.round(v.DiamondCost / (v.Item["_data"]["_am"] or 1)) <= SnipingID.MAX_PRICE and GetDiamonds() >= v.DiamondCost and MeetsForm(GetPetForm(v.Item["_data"]), SnipingID.FORM) then
